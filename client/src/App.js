dfa272b543120f17da710c053fa85706b1356529
Shahi77 <shahiinfinity02@gmail.com>
2025-02-03 09:26:22 +0530

    frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import CodeRunner from "./CodeRunner";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CodeRunner />
+    </div>
+  );
+}
+
+export default App;
