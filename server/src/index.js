32376bb99c7685cb47843917969461fe45a4018c
Shahi77 <shahiinfinity02@gmail.com>
2025-02-14 00:49:11 +0530

    add morgan

--- server/src/index.js
+++ server/src/index.js
@@ -22,6 +22,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.resolve("./public")));
 app.use(cookieParser());
+app.use(morgan("dev"));
 
 app.use("/v1", v1Router);
 // Start worker
